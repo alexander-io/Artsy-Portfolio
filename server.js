b3e4854c8d60ae04ca6cf7e2ce3cadf8d18de3f6
alexander-io <abharris@pugetsound.edu>
2017-10-07 13:18:40 -0700

    ++npm install --save materialize, define routes in server.js to respond to requests with res.sendFile

---
+++ server.js
@@ -0,0 +1,33 @@
+let express = require('express')
+let app = express()
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html')
+})
+
+app.get('jquery', function(req, res) {
+  res.sendFile(__dirname + '/node_modules/jquery/dist/jquery.js')
+})
+
+app.get('materialize.js', function(req, res) {
+  res.sendFile(__dirname + '/node_modules/materialize-css/dist/js/materialize.js')
+})
+
+app.get('materialize.css', function(req, res) {
+  res.sendFile(__dirname + '/node_modules/materialize-css/dist/css/materialize.css')
+})
+
+app.get('fonts/roboto/Roboto-Medium.woff', function(req, res) {
+  res.sendFile(__dirname + '/node_modules/materialize-css/dist/fonts/roboto/Roboto-Medium.woff')
+})
+
+app.get('fonts/roboto/Roboto-Medium.woff2', function(req, res) {
+  res.sendFile(__dirname + '/node_modules/materialize-css/dist/fonts/roboto/Roboto-Medium.woff2')
+})
+
+// node_modules/materialize-css/dist/fonts/roboto/Roboto-Medium.woff
+// node_modules/materialize-css/dist/fonts/roboto/Roboto-Medium.woff2
+
+app.listen(8080, function() {
+  console.log('listening on 8080');
+})

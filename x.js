27f75febb59c3b5a52cf74b0a2aaf2f0888cf8da
alexander-io <abharris@pugetsound.edu>
2017-10-07 13:05:35 -0700

    ++define classes for piece, medium, dimension; instantiate one member of this class

--- x.js
+++ x.js
@@ -1,12 +1,5 @@
 let clay_yay = null
 
-let piece = {
-  title : clay_yay,
-  medium : clay_yay /* clay body, glaze choice */,
-  year : clay_yay,
-  dimensions : clay_yay
-}
-
 class Medium {
   constructor(clay_body, glaze) {
     this.clay_body = clay_body
@@ -22,7 +15,42 @@ class Dimension {
 }
 
 class Piece {
-  constructor(title, medium, year, dimensions) {
-
+  constructor(title, medium, year, dimension) {
+    this.title = title
+    this.medium = medium
+    this.year = year
+    this.dimension = dimension
   }
 }
+
+// amazeBALLS, we've defined classes for the piece, dimension, and medium
+// a single instantiated piece object will have thjis structure
+/*
+let piece = {
+  title = 'son of a serpent',
+  medium = {
+    clay_body = 'porcelain',
+    glaze = 'celedon'
+  },
+  year = '3005',
+  dimension = {
+    height = '666in',
+    width = '-666in'
+  }
+}
+*/
+
+let piece = new Piece(
+  /* title : */ 'son of a serpent',
+  /* year : */ 2017,
+  /* medium : */ new Medium(
+  /* clay_body : */ 'porcelain',
+  /* glaze */ 'celedon'
+),
+  /* dimension : */ new Dimension(
+    /* height */ '666in',
+    /* width : */ '666in'
+  )
+)
+
+console.log(piece);

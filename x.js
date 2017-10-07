137a1b6e5f45ce9a449a7ae410ce1c685f78731f
alexander-io <abharris@pugetsound.edu>
2017-10-07 12:53:54 -0700

    init repository, begin modeling data structures in x.js

---
+++ x.js
@@ -0,0 +1,28 @@
+let clay_yay = null
+
+let piece = {
+  title : clay_yay,
+  medium : clay_yay /* clay body, glaze choice */,
+  year : clay_yay,
+  dimensions : clay_yay
+}
+
+class Medium {
+  constructor(clay_body, glaze) {
+    this.clay_body = clay_body
+    this.glaze = glaze
+  }
+}
+
+class Dimension {
+  constructor(height, width) {
+    this.height = height
+    this.width = width
+  }
+}
+
+class Piece {
+  constructor(title, medium, year, dimensions) {
+
+  }
+}

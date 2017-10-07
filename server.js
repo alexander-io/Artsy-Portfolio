ab2787054cd37a0a028bf732ab9d7ceb33c842b9
alexander-io <abharris@pugetsound.edu>
2017-10-07 13:22:50 -0700

    ++finish defining routes to fonts, apply section:container:row:column structure in markup

--- server.js
+++ server.js
@@ -5,24 +5,24 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/public/index.html')
 })
 
-app.get('jquery', function(req, res) {
+app.get('/jquery.js', function(req, res) {
   res.sendFile(__dirname + '/node_modules/jquery/dist/jquery.js')
 })
 
-app.get('materialize.js', function(req, res) {
+app.get('/materialize.js', function(req, res) {
   res.sendFile(__dirname + '/node_modules/materialize-css/dist/js/materialize.js')
 })
 
-app.get('materialize.css', function(req, res) {
+app.get('/materialize.css', function(req, res) {
   res.sendFile(__dirname + '/node_modules/materialize-css/dist/css/materialize.css')
 })
 
-app.get('fonts/roboto/Roboto-Medium.woff', function(req, res) {
-  res.sendFile(__dirname + '/node_modules/materialize-css/dist/fonts/roboto/Roboto-Medium.woff')
+app.get('/fonts/roboto/Roboto-Regular.woff', function(req, res) {
+  res.sendFile(__dirname + '/node_modules/materialize-css/dist/fonts/roboto/Roboto-Regular.woff')
 })
 
-app.get('fonts/roboto/Roboto-Medium.woff2', function(req, res) {
-  res.sendFile(__dirname + '/node_modules/materialize-css/dist/fonts/roboto/Roboto-Medium.woff2')
+app.get('/fonts/roboto/Roboto-Regular.woff2', function(req, res) {
+  res.sendFile(__dirname + '/node_modules/materialize-css/dist/fonts/roboto/Roboto-Regular.woff2')
 })
 
 // node_modules/materialize-css/dist/fonts/roboto/Roboto-Medium.woff
